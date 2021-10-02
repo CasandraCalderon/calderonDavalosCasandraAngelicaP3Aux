20cf6b0355f68e02884e075afed093207965c50f
Casandra Calderon <angie200896@gmail.com>
2021-10-01 23:04:02 -0400

    Tarea Nro 4 MODIFICADO

--- script.js
+++ script.js
@@ -8,19 +8,20 @@ const poll = {
     question: "What is your favourite programming language?",
     options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
     answers: new Array(4).fill(0),
+    registerNewAnswer : function () {
+        const newList = this.options.map(function(codes){return (codes + '\n')});
+        const number = prompt (`${this.question} \n ${newList.toString().replace(/,/g, "")}`, "");
+        (number < this.options.length && number >= 0) ? 
+        this.answers[number] = this.answers[number] +1 : console.log("Coloque un numero que se encuentre dentro de las opciones");
+        const displayResults = (type) => {
+            return (`Los resultados de la encuesta son ${type.toString()} respectivamente`);
+        };
+        console.log(displayResults(this.answers));
+    }   
 };
 
 
-const registerNewAnswer = () =>{
-    const newList = poll.options.map(function(codes){return (codes + '\n')});
-    const number = prompt (`${poll.question} \n ${newList.toString().replace(/,/g, "")}`, "");
-    (number < poll.options.length && number >= 0) ? 
-    poll.answers[number] = poll.answers[number] +1 : console.log("Coloque un numero que se encuentre dentro de las opciones");
-    const displayResults = (type) => {
-        return (`Los resultados de la encuesta son ${type.toString()} respectivamente`);
-    };
-    return (console.log(displayResults(poll.answers)));
-    
-};
+
+
 
 
